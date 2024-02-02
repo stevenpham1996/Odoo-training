2e2165a9a9ea18610562f8e530de2bab21ff7890
Pham <stevenpham1996@outlook.com>
2024-02-02 18:44:55 +0700

    Finish chapter 9

--- technical-training/estate/models/estate_offer.py
+++ technical-training/estate/models/estate_offer.py
@@ -1,13 +1,15 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from dateutil.relativedelta import relativedelta
 
 
 class EstateOffer(models.Model):
     _name = "estate.offer"
-    _description = "Real Estate Offers"
+    _description = "Property Offers"
 
     # --------------------------------------- Fields Declaration ----------------------------------
 
     price = fields.Float("Offered Price", required=True)
+    validity = fields.Integer(string="Validity (days)", default=7)  
     status = fields.Selection(
         selection=[
             ("accepted", "Accepted"),
@@ -19,3 +21,18 @@ class EstateOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+    # Computed Fields
+    date_deadline = fields.Date(compute="_compute_deadline_date", inverse="_inverse_deadline_date", string="Deadline (days)")
+
+    # --------------------------------------- Compute Methods ----------------------------------
+
+    @api.depends("create_date", "validity")
+    def _compute_deadline_date(self):
+        for record in self:
+            date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = date + relativedelta(days=record.validity)
+            
+    def _inverse_deadline_date(self):
+        for record in self:
+            date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - date).days
--- technical-training/estate/models/estate_property.py
+++ technical-training/estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from dateutil.relativedelta import relativedelta
 
 
@@ -69,6 +69,34 @@ class EstateProperty(models.Model):
         default=lambda self: self.env.user,
     )
     tag_ids = fields.Many2many("estate.tag", string="Tags", index=True)
-    offer_ids = fields.One2many(
-        "estate.offer", "property_id", string="Offers"
-    )
+    offer_ids = fields.One2many("estate.offer", "property_id", string="Offers")
+
+    # --------------------------------------- Calculated Fields ----------------------------------
+
+    total_area = fields.Float(compute="_compute_total_area", string="Total Area")
+    best_price = fields.Float(compute="_compute_best_price", string="Best Price")
+
+    # --------------------------------------- Compute Methods ----------------------------------
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped("price"))
+            else:
+                record.best_price = record.expected_price
+
+    # --------------------------------------- Onchange Methods ----------------------------------
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "N"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
